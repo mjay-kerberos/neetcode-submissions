4cfe1ee52913d2f3867ea9f2be615eefb942c5fa
Juliet Meza <juliet.alexa.meza@gmail.com>
2026-07-31 18:36:59 -0400

    Add: python-tuples - submission-0

---
+++ Python For Beginners/python-tuples/submission-0.py
@@ -0,0 +1,11 @@
+from typing import Tuple # this is to add type hints for tuples
+
+def create_pair(name: str, age: int) -> Tuple[str, int]:
+    info = (name, age)
+    return info
+
+
+# do not modify code below this line
+print(create_pair("Alice", 25))
+print(create_pair("Bob", 30))
+print(create_pair("Charlie", 35))
